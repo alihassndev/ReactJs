9f7e735ad559b9957637438ee84435ce51b3eb35
Ali Hassan <alihassndev@gmail.com>
2024-11-30 12:26:05 +0500

    getting started and folder structure

---
+++ first-vite-react/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1>Start React Journey with Vite</h1>
+      <p className="read-the-docs">Ali Hassan Start Learning ReactJs</p>
+    </>
+  );
+}
+
+export default App;
