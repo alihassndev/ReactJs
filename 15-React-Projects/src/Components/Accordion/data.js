d181e751cd1042c0ca36a3b514b1e4a9d2a661b9
Ali Hassan <alihassndev@gmail.com>
2024-12-23 23:11:16 +0500

    Accordion component

---
+++ 15-React-Projects/src/Components/Accordion/data.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    id: 1,
+    question: "What are accordion components?",
+    answer:
+      "Accordion components are UI elements that allow users to expand and collapse sections of content to save space and improve readability.",
+  },
+  {
+    id: 2,
+    question: "What are they used for?",
+    answer:
+      "Accordion components are UI elements that allow users to expand and collapse sections of content to save space and improve readability.",
+  },
+  {
+    id: 3,
+    question: "Accordion are musical instrument?",
+    answer:
+      "Accordion components are UI elements that allow users to expand and collapse sections of content to save space and improve readability.",
+  },
+  {
+    id: 4,
+    question: "Can I create an accordion component with a different framework?",
+    answer:
+      "Accordion components are UI elements that allow users to expand and collapse sections of content to save space and improve readability.",
+  },
+];
+
+export default data;
